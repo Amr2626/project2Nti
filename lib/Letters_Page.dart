e710461ea4b98ca4fb67080e664b7eef1009ad53
project <platinum@MacBook-Pro-platinum.local>
2025-07-16 15:06:59 -0700

    first commit

--- lib/Letters_Page.dart
+++ lib/Letters_Page.dart
@@ -1,22 +1,22 @@
-// import 'package:flutter/material.dart';
-// import 'package:task2nti/letters&numbers_list.dart';
-// import 'package:task2nti/models/letters.dart';
+import 'package:flutter/material.dart';
+import 'package:task2nti/letters&numbers_list.dart';
+import 'package:task2nti/models/letters.dart';
 
 
-// class LettersPage extends StatelessWidget {
-//   const LettersPage({super.key});
+class LettersPage extends StatelessWidget {
+  const LettersPage({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: const Text("Letters")),
-//       body: GridView.count(
-//         crossAxisCount: 4,
-//         padding: const EdgeInsets.all(8),
-//         mainAxisSpacing: 8,
-//         crossAxisSpacing: 8,
-//         children: letterItems.map((item) => LetterBox(letterItem: item)).toList(),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Letters")),
+      body: GridView.count(
+        crossAxisCount: 4,
+        padding: const EdgeInsets.all(8),
+        mainAxisSpacing: 8,
+        crossAxisSpacing: 8,
+        children: letterItems.map((item) => LetterBox(letterItem: item)).toList(),
+      ),
+    );
+  }
+}
